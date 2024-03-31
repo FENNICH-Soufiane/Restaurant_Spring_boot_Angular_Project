2715d36cb37d1fc4f814937d9e2260a814e882bb
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-03-31 15:48:29 +0000

    implementer Lombok

--- back_end/src/main/java/com/fstg/restaurant/model/Category.java
+++ back_end/src/main/java/com/fstg/restaurant/model/Category.java
@@ -1,9 +1,13 @@
 package com.fstg.restaurant.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+
 public class Category extends CategoryOrder  {
 	
-	
-	
-	
+		
 
 }
--- back_end/src/main/java/com/fstg/restaurant/model/CategoryOrder.java
+++ back_end/src/main/java/com/fstg/restaurant/model/CategoryOrder.java
@@ -1,8 +1,13 @@
 package com.fstg.restaurant.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // cette class contient name et id
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryOrder extends BaseEntity {
 
 	private String name;
