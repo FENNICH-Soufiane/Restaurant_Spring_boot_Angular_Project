8741e1524913d67cf6cce8d72acf3dceac2fbbf9
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-03-31 15:14:09 +0000

    fill model (Category & Order)

--- back_end/src/main/java/com/fstg/restaurant/model/Category.java
+++ back_end/src/main/java/com/fstg/restaurant/model/Category.java
@@ -1,5 +1,9 @@
 package com.fstg.restaurant.model;
 
 public class Category {
+	
+	
+	private Long id;
+	private String categoryName;
 
 }
--- back_end/src/main/java/com/fstg/restaurant/model/Order.java
+++ back_end/src/main/java/com/fstg/restaurant/model/Order.java
@@ -2,4 +2,11 @@ package com.fstg.restaurant.model;
 
 public class Order {
 
+	
+	private Long id;
+	private String nameOrder;
+	private int price;
+	private String img;
+	private String description;
+	
 }
