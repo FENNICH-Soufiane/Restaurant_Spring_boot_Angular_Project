a8624350e62057344a27629a1951d07822d94a68
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-04-01 10:16:28 +0000

    add @CreationTimestamp @UpdateTimestamp

--- back_end/src/main/java/com/fstg/restaurant/model/CategoryOrder.java
+++ back_end/src/main/java/com/fstg/restaurant/model/CategoryOrder.java
@@ -1,5 +1,10 @@
 package com.fstg.restaurant.model;
 
+import java.sql.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
@@ -15,4 +20,13 @@ public class CategoryOrder extends BaseEntity {
 
 	@Column(name = "name")
 	private String name;
+	
+	@Column(name = "date_create")
+	@CreationTimestamp
+	private Date dataCreate;
+	
+	@Column(name = "date_update")
+	@UpdateTimestamp
+	private Date dataUpdate;
+	
 }
