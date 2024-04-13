6270e05a1344350f261f54ef0db5af350b4361cf
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-04-13 18:32:47 +0000

    .

--- back_end/src/main/java/com/fstg/restaurant/service/CategoryService.java
+++ back_end/src/main/java/com/fstg/restaurant/service/CategoryService.java
@@ -1,5 +1,16 @@
 package com.fstg.restaurant.service;
 
-public class CategoryService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.fstg.restaurant.dao.CategoryRepository;
+
+@Service
+public class CategoryService {
+	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	public CategoryService(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
 }
--- back_end/src/main/java/com/fstg/restaurant/service/OrderService.java
+++ back_end/src/main/java/com/fstg/restaurant/service/OrderService.java
@@ -1,5 +1,21 @@
 package com.fstg.restaurant.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fstg.restaurant.dao.OrderRepository;
+
+@Service
 public class OrderService {
 
+	private OrderRepository orderRepository;
+
+	@Autowired
+	public OrderService(OrderRepository orderRepository) {
+		super();
+		this.orderRepository = orderRepository;
+	}
+	
+
+	
 }
