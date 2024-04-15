88a15797c5c4711af43c95dd5290f3b59636c2df
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-04-15 16:43:18 +0100

    Angular Show Data in Order-Item Component

--- front_end/src/app/app.component.ts
+++ front_end/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OrderItemsComponent } from "./components/order-items/order-items.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, OrderItemsComponent]
 })
 export class AppComponent {
   title = 'front_end';
